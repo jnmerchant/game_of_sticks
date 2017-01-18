f360e22c0c92946249e18f072d7a61fa786027eb
jnmerchant <joemercnc@gmail.com>
2017-01-18 16:53:51 -0500

    update with working next player turn method

--- lib/game_of_sticks.rb
+++ lib/game_of_sticks.rb
@@ -3,19 +3,25 @@ class Game
     @number_of_players = number_of_players
     @number_of_sticks = number_of_sticks
     @number_sticks_remaining = number_of_sticks
-    @current_player_turn = ''
+    @player_turn = ''
   end
 
   attr_reader :number_of_players
   attr_reader :number_of_sticks
   attr_accessor :number_sticks_remaining
+  attr_accessor :player_turn
 
   def get_remaining_sticks(sticks_in_turn)
     @number_sticks_remaining -= sticks_in_turn
   end
 
-  def whos_turn_is_it
-    if @current_player_turn == ''
+  def next_players_turn
+    players = ["Player1","Player2"]
+    if @player_turn == '' && @number_of_players == 2
+      @player_turn = players.sample
+    else
+      @player_turn == players[0] ? @player_turn = players[1] : @player_turn = players[0]
+    end
   end
 
   def to_s
--- spec/game_of_sticks_spec.rb
+++ spec/game_of_sticks_spec.rb
@@ -9,20 +9,29 @@ describe "Game.new" do
 
 describe "#get_remaining_sticks" do
   context "#return the number of sticks remaining after a players turn" do
-    it "#reduces the re" do
+    it "#reduces the remaining sticks by the amount taken" do
       game = Game.new(2, 50)
       expect(game.get_remaining_sticks(3)).to be 47
     end
   end
 end
 
-describe "#get_remaining_sticks" do
-  context "#return the number of sticks remaining after a players turn" do
-    it "#reduces the re" do
+describe "#next_players_turn" do
+  context "#determine next_players_turn should be set to Player1 when its current value is Player2" do
+    it "#determines next_players_turn when player_turn == Player1" do
       game = Game.new(2, 50)
-      expect(game.get_remaining_sticks(3)).to be 47
+      game.player_turn = 'Player1'
+      expect(game.next_players_turn).to eq 'Player2'
+    end
+
+    it "#determines next_players_turn when player_turn == Player2" do
+      game = Game.new(2, 50)
+      game.player_turn = 'Player2'
+      expect(game.next_players_turn).to eq 'Player1'
     end
   end
 end
+
+
 
 end
